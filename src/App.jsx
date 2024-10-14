50cc05706b48b67d3ede78dfe9a24321d370d37f
Nikhil Kalamkar <kalamkarnikhil012@gmail.com>
2024-10-14 23:34:44 +0530

    edit some changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
       <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} />s */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
